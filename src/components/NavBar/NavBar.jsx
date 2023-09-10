955a22e93d38a9dbb945d7b3dbc0a355abbff2fd
Joaquingro <joaquinsgro@gmail.com>
2023-09-10 15:30:49 -0600

    Hamburguer button fixed in responsive dark

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -17,7 +17,7 @@ function NavBar({isDarkMode, handleDarkMode}) {
       <img src={logo} alt="logo" />
       <h1>GrosGar</h1>
     </div>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <button className={isDarkMode ? `navbar-toggler ${style.dark2}` : `navbar-toggler ${style.hamburguer}`} type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
   <div className={`collapse navbar-collapse ${style.containerButtons}`} id="navbarSupportedContent">
