6f96cb3bf9526c8058626048922f528e5a59bba8
Joaquingro <joaquinsgro@gmail.com>
2023-09-18 00:41:53 -0600

    Changes in ServiceDetail, drop one service

--- src/pages/ServiceDetail/Service.jsx
+++ src/pages/ServiceDetail/Service.jsx
@@ -3,10 +3,10 @@ let ServicioInfo = [
        name: "Servicio",
        description: "Estamos trabajando en los mejores servicios para usted..."
     }, 
-    {
-        name: "Servicio",
-        description: "Estamos trabajando en los mejores servicios para usted..." 
-     }, 
+   //  {
+   //      name: "Servicio",
+   //      description: "Estamos trabajando en los mejores servicios para usted..." 
+   //   }, 
    //   {
    //      name: "Servicio",
    //      description: "Estamos trabajando en los mejores servicios para usted..." 
