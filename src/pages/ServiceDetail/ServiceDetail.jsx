b29eafe89b0634f6afc399f4c2ea574d2980b0ba
Joaquin Guerrero <joaquinsgro@gmail.com>
2023-12-17 20:14:45 -0600

    Add responsive and CSS finished

--- src/pages/ServiceDetail/ServiceDetail.jsx
+++ src/pages/ServiceDetail/ServiceDetail.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import style from "../ServiceDetail/ServiceDetail.module.css";
 import serviceInfo from './Service';
 import Footer from '../../components/Footer/Footer';
@@ -38,18 +38,18 @@ export default function ServiceDetail({isDarkMode}) {
       aria-hidden="true"
     >
       <div
-        className={
-          isDarkMode ? `modal-dialog ${style.dark4}` : `modal-dialog ${style.modalContainer}`
-        }
+        className="modal-dialog"
       >
-        <div className="modal-content">
+        <div className={
+          isDarkMode ? `modal-content ${style.dark3}` : `modal-content ${style.modalContainer}`
+        }>
           <div className="modal-header">
             <h1 className="modal-title fs-5" id={`exampleModalLabel-${index}`}>
               <h3>{service.name}</h3>
               </h1>
             <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
-          <div className={`modal-body ${style.modalText}`}>
+          <div className={isDarkMode ? `modal-body ${style.dark4}` : `modal-body ${style.modalText}`}>
             {service.resume}
 
           <h4>¿Te interesa contratar este servicio o saber más?</h4>
@@ -57,7 +57,9 @@ export default function ServiceDetail({isDarkMode}) {
           </div>
           
           <div className="modal-footer">
-            
+          <a
+            href={`https://wa.me/5532943994/?text=Hola, estoy interesado en el servicio de ${encodeURIComponent(service.name)}. ¿Puede proporcionarme más información?`}
+          />
             <button type="button" className="btn btn-success">
             <i class="bi bi-whatsapp"></i> Contratar Servicio
             </button>
